be36640a6616eac42562fab8ed79f8b318331ee9
Gerai2963 <85170301+Gerai2963@users.noreply.github.com>
2021-06-10 18:08:23 +0100

    print pull_request body

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+  console.log('/n/nThe body of this PR is: ${github.context.payload.pull_request.body}');
 } catch (error) {
   core.setFailed(error.message);
 }
