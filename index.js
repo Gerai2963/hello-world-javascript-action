3930aa8915a7d6e422fd283c563177e80a34c538
Gerai2963 <85170301+Gerai2963@users.noreply.github.com>
2021-06-10 18:13:25 +0100

    separated lines of body

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
-  console.log('/n/nThe body of this PR is: ${github.context.payload.pull_request.body}');
+  console.log('The body of this PR is:');
+  console.log('${github.context.payload.pull_request.body}');
 } catch (error) {
   core.setFailed(error.message);
 }
